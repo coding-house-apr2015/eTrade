aa372ff1a392e00eed6f926beb7138062b4c6254
Chyld Medford <chyld.medford@gmail.com>
2015-04-28 10:00:31 -0700

    twitter:

--- client/models/user.js
+++ client/models/user.js
@@ -6,6 +6,10 @@ angular.module('eTrade')
   function User(){
   }
 
+  User.oauth = function(provider){
+    return $rootScope.afAuth.$authWithOAuthPopup(provider);
+  };
+
   User.register = function(user){
     return $rootScope.afAuth.$createUser(user);
   };
