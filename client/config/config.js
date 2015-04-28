375e281d449b300b4c03a5325a0ad09f551908b5
Chyld Medford <chyld.medford@gmail.com>
2015-04-28 14:24:09 -0700

    portfolios route

--- client/config/config.js
+++ client/config/config.js
@@ -11,5 +11,9 @@ angular.module('eTrade')
   .state('contact', {url: '/contact', templateUrl: '/views/general/contact.html'})
   .state('register', {url: '/register', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
   .state('login', {url: '/login', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
-  .state('profile', {url: '/profile', templateUrl: '/views/profile/profile.html', controller: 'ProfileCtrl'});
+  .state('profile', {url: '/profile', templateUrl: '/views/profile/profile.html', controller: 'ProfileCtrl'})
+  .state('portfolios', {url: '/portfolios', templateUrl: '/views/portfolios/portfolios.html', abstract: true})
+  .state('portfolios.list', {url: '', templateUrl: '/views/portfolios/portfolios-list.html', controller: 'PortfoliosListCtrl'})
+  .state('portfolios.new', {url: '/new', templateUrl: '/views/portfolios/portfolios-new.html', controller: 'PortfoliosNewCtrl'})
+  .state('portfolios.show', {url: '/{name}', templateUrl: '/views/portfolios/portfolios-show.html', controller: 'PortfoliosShowCtrl'});
 });
---
+++ client/views/portfolios/portfolios-show.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('eTrade')
+.controller('PortfoliosShowCtrl', function(){
+});
