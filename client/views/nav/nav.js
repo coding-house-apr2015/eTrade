42b74844430c12d606ccc05770b2e6f787135b5c
Chyld Medford <chyld.medford@gmail.com>
2015-04-28 10:09:15 -0700

    auth data

--- client/views/nav/nav.js
+++ client/views/nav/nav.js
@@ -6,8 +6,10 @@ angular.module('eTrade')
   $scope.afAuth.$onAuth(function(data){
     if(data){
       $rootScope.activeUser = data;
+      $rootScope.displayName = getDisplayName(data);
     }else{
       $rootScope.activeUser = null;
+      $rootScope.displayName = null;
     }
 
     $state.go('home');
@@ -16,4 +18,13 @@ angular.module('eTrade')
   $scope.logout = function(){
     User.logout();
   };
+
+  function getDisplayName(data){
+    switch(data.provider){
+      case 'password':
+        return data.password.email;
+      case 'twitter':
+        return data.twitter.username;
+    }
+  }
 });
